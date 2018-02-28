301c0df29aa3783092b4dbb6b8e6f0c4bf86707d
yanqirenshi <yanqirenshi@gmail.com>
2018-02-28 14:24:58 +0900

    add logs class

---
+++ src/logs/class.lisp
@@ -0,0 +1,8 @@
+(in-package :ahan-whun-shugoi)
+
+;; '(:|logGroups| 
+;;   ((:|storedBytes| {integer}
+;;     :|logGroupName| "..."
+;;     :|metricFilterCount| {integer}
+;;     :|creationTime| {integer}
+;;     :|arn| "...")))
