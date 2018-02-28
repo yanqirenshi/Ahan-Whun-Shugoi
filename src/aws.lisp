00db63426975f4381c78719ef6b30e62ad26259c
yanqirenshi <yanqirenshi@gmail.com>
2018-02-28 09:26:28 +0900

    add opt2cmd

--- src/aws.lisp
+++ src/aws.lisp
@@ -16,16 +16,39 @@
   (assert (keywordp service))
   (assoc service *commands*))
 
-(defun get-command-options (service cmd)
-  (cadr (assoc cmd (cadr (assoc service *commands*)))))
+(defun options-table ()
+  '((:--profile 'string)))
+
+(defun get-option-values (options)
+  (let ((first-keyword-position (position-if (lambda (v) (keywordp v)) options)))
+    (if (null first-keyword-position)
+        options
+        (subseq options 0 first-keyword-position))))
+
+(defun assert-option-values (option-table-values option-values)
+  (assert (= (length option-table-values)
+             (length option-values))))
+
+(defun opt2cmd (options &key (options-table (options-table)))
+  (when options
+    (let* ((option (car options))
+           (option-table (assoc option options-table)))
+      (assert (keywordp option))
+      (assert option-table)
+      (let ((option-table-values (cdr option-table))
+            (option-values (get-option-values (cdr options))))
+        (assert-option-values option-table-values option-values)
+        (concatenate 'string
+                     (format nil "~(~a~) ~{~a ~}" option option-values)
+                     (opt2cmd (subseq options (+ 1 (length option-values)))))))))
 
 (defun make-aws-command (service command &rest options)
-  (let ((options (get-command-options service command)))
-    (declare (ignore options))
-    (format nil "aws ~a ~a ~a"
-            (string-downcase (symbol-name service))
-            (string-downcase (symbol-name command))
-            (first options))))
+  (format nil "aws ~a ~a ~a"
+          (string-downcase (symbol-name service))
+          (string-downcase (symbol-name command))
+          (if options
+              (opt2cmd options)
+              "")))
 
 (defun valudation-service (service)
   (unless (servicep service)
@@ -34,6 +57,7 @@
 (defun aws (service command &rest options)
   (assert (servicep service))
   (let ((cmd (apply #'make-aws-command service command options)))
+    (print cmd)
     (multiple-value-bind (values output error-output exit-status)
         (trivial-shell:shell-command cmd)
       (declare (ignore output error-output exit-status))
