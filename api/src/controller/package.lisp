8324d526cbb1db5280ea71706c58a70d90a08e23
yanqirenshi <yanqirenshi@gmail.com>
2018-03-20 15:50:31 +0900

    rm print

--- api/src/controller/package.lisp
+++ api/src/controller/package.lisp
@@ -21,7 +21,6 @@
   (let ((vertexs nil)
         (relationship nil))
     (dolist (plist (shinra:find-r graph to-class :from from-vertex))
-      (print (getf plist :vertex))
       (push (getf plist :vertex) vertexs)
       (push (getf plist :edge) relationship))
     (list :nodes vertexs :relationships relationship)))
