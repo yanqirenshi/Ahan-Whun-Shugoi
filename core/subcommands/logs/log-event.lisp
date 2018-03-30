89d690e38cfd26c6b3d5a466ce3b37eabe872163
yanqirenshi <yanqirenshi@gmail.com>
2018-03-30 13:38:27 +0900

    remove core/subcommands

--- core/subcommands/logs/log-event.lisp
+++
@@ -1,9 +0,0 @@
-(in-package :ahan-whun-shugoi)
-
-(defun plist2log-event (plist)
-  (make-instance 'log-event
-                 :log-stream-name (getf plist :|logStreamName|)
-                 :event-id (getf plist :|eventId|)
-                 :message (getf plist :|message|)
-                 :timestamp (unix-time2timestamp (getf plist :|timestamp|))
-                 :ingestion-time (unix-time2timestamp (getf plist :|ingestionTime|))))
